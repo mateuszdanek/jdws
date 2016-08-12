66cc5864792923febff4db7b1462f2534f395592
mateuszdanek <mateuszdanek@gmail.com>
2016-08-12 16:56:40 +0200

    Rectangle added

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+var gamePiece;
+
 function startGame () {
-	myGameArea.start();
+	gameArea.start();
+	gamePiece = new component(30, 30, "red", 10, 120);
 }
 
-var myGameArea = {
+var gameArea = {
 	canvas : document.createElement("canvas"),
 	start : function () {
-		this.canvas.width="480";
-		this.canvas.height="270";
-		this.canvas.context=this.canvas.getContext("2d");
+		this.canvas.width=480;
+		this.canvas.height=270;
+		this.context=this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 	}
 };
+
+function component (width, height, color, x, y) {
+	this.width = width;
+	this.height = height;
+	this.x = x;
+	this.y = y;
+	ctx = gameArea.context;
+	ctx.fillStyle = color;
+	ctx.fillRect(this.x, this.y, this.width, this.height);
+}
