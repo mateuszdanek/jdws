b8e3e83157c2b8b70fcfbff3276a36133776dc76
mateuszdanek <mateuszdanek@gmail.com>
2016-08-21 18:23:35 +0200

    Rectangle movement and control added

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ var gameArea = {
 		this.canvas.height=270;
 		this.context=this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea,20);
+	},
+	clear: function () {
+		this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
 	}
 };
 
@@ -20,7 +24,42 @@ function component (width, height, color, x, y) {
 	this.height = height;
 	this.x = x;
 	this.y = y;
-	ctx = gameArea.context;
-	ctx.fillStyle = color;
-	ctx.fillRect(this.x, this.y, this.width, this.height);
+	this.speedX = 0;
+	this.speedY = 0;
+	this.update = function () {
+		ctx = gameArea.context;
+		ctx.fillStyle = color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	};
+	this.newPos = function () {
+		this.x += this.speedX;
+		this.y += this.speedY;
+	};
+}
+
+function updateGameArea () {
+	gameArea.clear();
+	gamePiece.newPos();
+	gamePiece.update();
+}
+
+function moveUp () {
+	gamePiece.speedY -= 1;
+}
+
+function moveDown () {
+	gamePiece.speedY =+ 1;
+}
+
+function moveLeft () {
+	gamePiece.speedX -= 1;
+}
+
+function moveRight () {
+	gamePiece.speedX += 1;
+}
+
+function stopMove() {
+	gamePiece.speedX = 0;
+	gamePiece.speedY = 0;
 }
